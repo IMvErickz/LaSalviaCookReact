738c050ec9f11b5fabc614dcff2208ad3eb05630
IMvErickz <erickspy2003@gmail.com>
2023-01-02 18:44:05 -0300

    fixed: removed for and added a .map

--- src/components/Recepies/bodyRecepie.tsx
+++ src/components/Recepies/bodyRecepie.tsx
@@ -1,8 +1,37 @@
 import { Link } from "react-router-dom";
+import { api } from "../../../lib/axios";
 import { Header } from "../Header/Header";
 
 
 export function BodyRecepie() {
+
+     const getTittleRecepie = localStorage.getItem("ValButton")
+    //console.log(getTittleRecepie)
+
+    function setBodyRecepie() {
+        api.get('recepies/count')
+            .then(function (response) {
+            let res = response.data.count
+                let div = (document.getElementById('blockRecepie') as HTMLElement)
+            
+                for (let i = 0; i < res.length; i++) {
+                
+                    let transformTitle = response.data.count[i].tittle 
+                    let transformbody = response.data.count[i].body
+
+        
+                    //console.log(JSON.stringify(transformTitle))
+
+                    // const paragraph = document.createElement('p') 
+                    // paragraph.innerHTML = transformTitle
+                    // const pp = (document.getElementById('p') as HTMLElement)
+                    // pp.appendChild(paragraph)
+                }
+        })
+    }
+
+    setBodyRecepie()
+
     return (
         <div className="w-screen h-screen flex flex-col items-start justify-start bg-background">
             <div className="flex flex-col items-start justify-start">
@@ -11,11 +40,11 @@ export function BodyRecepie() {
 
             <div className="flex flex-col items-center justify-center w-full h-full">
                 <Header />
-                <h1 className="text-white text-6xl font-apple">Título da Receita</h1>
+                <h1 className="text-white text-6xl font-apple">{getTittleRecepie }</h1>
             </div>
 
             <div className="flex flex-col items-center justify-center w-full h-full bg-[#1f2937]">
-                <p className="text-white text-2xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad magnam, unde tempore, labore modi sit assumenda illum</p>
+                <p className="text-white text-2xl" id="p"></p>
             </div>
             
         </div>
--- src/components/Recepies/recepies.tsx
+++ src/components/Recepies/recepies.tsx
@@ -2,62 +2,27 @@ import React, { useState } from 'react'
 import Menu from '../../style/menu'
 import { Link } from 'react-router-dom'
 import { api } from '../../../lib/axios'
-import CSS from 'csstype';
 
-
+interface RecepieProps{
+    tittle: string
+}
 
 export default function App() {
 
-    
+    const [recepie, getRecepie] = useState<RecepieProps[]>([])
 
     function recRecepie(){
         api.get('recepies/count')
-        
-    
-            .then(function (response) {
-        
-                let res = response.data.count
-                let div = (document.getElementById('blockRecepie') as HTMLElement)
             
-                for (let i = 0; i < res.length; i++) {
-                
-                    let transformTitle = response.data.count[i].tittle
-        
-                    console.log(JSON.stringify(transformTitle))
-                
-                    let t = document.createElement('button')
-                    t.value = transformTitle
-                    t.style.color = 'white'
-                    t.style.fontWeight = 'bold'
-                    t.style.fontSize = '30px'
-                    t.id = "openModal"
-                    t.style.backgroundColor = 'black'
-                    t.style.borderRadius = '8px'
-                    t.style.width = "400px"
-                    t.innerHTML = transformTitle
-
-                    div.appendChild(t)
-                }
-
-                const buttonClicked = (document.getElementById('openModal') as HTMLButtonElement)
-                buttonClicked.addEventListener("click", () => {
-                    const buttonValue = (document.getElementById('openModal') as HTMLButtonElement).value 
-                    const setLocalStorage = localStorage.setItem("ValButton", buttonValue)
-                })
- 
-                
+            .then(function (response) {
+                getRecepie(response.data.count)
             })
     }
 
     recRecepie()
-        
-        
-
-   
-   
 
         return (
-            <div className=" w-screen h-screen bg-background flex flex-col items-center gap-y-5">
+            <div className="bg-background flex flex-col items-center gap-y-5">
                 <header className='flex flex-col items-center'>
                     <Menu className='w-40' />
                     <h1 className="font-cursive text-6xl text-tittle">Receitas</h1>
@@ -73,12 +38,15 @@ export default function App() {
                     <Link to="/AdcionarReceita"><button className='bg-tittle p-2 rounded-lg hover:bg-[#fde68a] font-bold w-full'>Adicione sua receita</button></Link>
                 </div>
 
-                <ul>
-                    <li  className='flex flex-col items-center justify-center'>
-                        
-                        <Link to='/receita' id='blockRecepie' className='flex flex-col items-center w-full h-full gap-y-5 '></Link>
-                        
-                    </li>
+                <ul className='flex flex-col itens-center justify-center gap-y-4'>
+                    {recepie.map(recepies => {
+                        return (
+                            <li className='flex flex-col items-center justify-center text-white text-2xl p-4 bg-black rounded-lg font-bold'>
+                                <Link to='/receita'>{recepies.tittle}</Link>
+                            </li>
+                        )
+                    })}
+                   
                 </ul>
 
             </div>
